59cd2e267c032546fc60dd5112d2d9cc86d2938d
Maksim Odnoletkov <odnoletkov@mail.ru>
2024-02-02 18:22:23 +0000

    Repro issues with presented tear down and dynamic colors encoding

--- ShareExtension/ShareViewController.swift
+++ ShareExtension/ShareViewController.swift
@@ -3,9 +3,30 @@ import Social
 import QuickLook
 import CoreServices
 
+var once = false
+
+var resolved = 0
+
 class ShareViewController: UINavigationController {
     override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
         super.init(rootViewController: RootController())
+
+        if !once {
+            let dynamicColor = UIColor { trait in
+                UIColor { trait in
+                    UIColor { trait in
+//                        print("read \(trait)")
+                        resolved += 1
+                        print("resolved \(resolved)")
+                        return UIColor.black
+                    }
+                }
+            }
+
+            UINavigationBar.appearance().titleTextAttributes = [.foregroundColor: dynamicColor]
+            UINavigationBar.appearance().largeTitleTextAttributes = UINavigationBar.appearance().titleTextAttributes
+            once = true
+        }
     }
     
     required init?(coder aDecoder: NSCoder) {
@@ -15,14 +36,21 @@ class ShareViewController: UINavigationController {
 
 class RootController: UIViewController {
     override func viewDidLoad() {
-        DispatchQueue.main.async {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
             let controller = CarouselViewController()
-            controller.modalPresentationStyle = .formSheet
             controller.navigationItem.rightBarButtonItem = .init(systemItem: .close, primaryAction: .init { [weak self] _ in
                 self?.extensionContext?.completeRequest(returningItems: nil)
             })
-            self.present(UINavigationController(rootViewController: controller), animated: true)
+            let navigationController = UINavigationController(rootViewController: controller)
+            navigationController.modalPresentationStyle = .formSheet
+            self.present(navigationController, animated: true)
         }
+    }
+
+    // Fix presented controller not dismissed during SE tear down
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        presentedViewController?.dismiss(animated: false)
     }
 }
 
